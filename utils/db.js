b977f88df786dfcd74d39ae2b125813142ce6710
Master_shiv.info.me <103109048+shivamkumarsha@users.noreply.github.com>
2024-05-28 00:55:34 +0530

    minnor dependency issue

--- utils/db.js
+++ utils/db.js
@@ -1,21 +1,21 @@
-import { config } from "dotenv";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-config();
+dotenv.config();
 
 const dbURI = process.env.DATABASE;
 
-
-
-
 const connectDb = async () => {
   try {
-    await mongoose.connect(dbURI);
+    await mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to database");
   } catch (err) {
-    console.log("database connection error",err);
-    process.exit(0);
+    console.log("database connection error", err);
+    process.exit(1);
   }
 };
 
-export default connectDb
+export default connectDb;
